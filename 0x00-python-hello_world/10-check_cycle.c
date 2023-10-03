ce83d875664597e20046a7b595130942404aedf5
khalildahbane <khalildahbne@gmail.com>
2023-10-03 13:55:16 -0700

    linked

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+
+
+int check_cycle(listint_t *list)
+{
+	listint_t *hera, *tort;
+
+	if (!list)
+		return (0);
+
+	while (hera && tort && tort->next)
+	{
+		hera = tort->next;
+		tort = tort->next->next;
+		if (hera == tort)
+			return (1);
+	}
+
+	return (0);
+}
