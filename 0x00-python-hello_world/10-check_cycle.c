761ed78685ad93bd2624f25bb668ce496c6282df
kelldani <87382954+khalildahbane@users.noreply.github.com>
2023-10-03 21:03:20 +0000

    linked

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,21 +1,20 @@
 #include "lists.h"
 
 
-
 int check_cycle(listint_t *list)
 {
-	listint_t *hera, *tort;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
 	if (!list)
 		return (0);
 
-	while (hera && tort && tort->next)
+	while (slow && fast && fast->next)
 	{
-		hera = tort->next;
-		tort = tort->next->next;
-		if (hera == tort)
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
 			return (1);
 	}
-
 	return (0);
 }
