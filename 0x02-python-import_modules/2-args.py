287c0236828e4b358c24ab696901622405129e0d
kelldani <87382954+khalildahbane@users.noreply.github.com>
2023-10-08 16:23:16 +0000

    fatal

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for x in range(count):
+        print("{}: {}".format(x + 1, sys.argv[x + 1]))
