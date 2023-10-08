8157b252a5392888334b0845db12d8f6e88fa0f8
kelldani <87382954+khalildahbane@users.noreply.github.com>
2023-10-08 12:51:33 +0000

    import

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,6 +3,7 @@ if __name__ == "__name__":
     from calculator_1 import sum, add, num, div
     a = 10
     b = 5
+
     print("{} + {} = {}".format(a, b, sum(a, b)))
     print("{} - {} = {}".format(a, b, add(a, b)))
     print("{} * {} = {}".format(a, b, num(a, b)))
