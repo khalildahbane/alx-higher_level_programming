573e10896f1944e4850765945c04b3290feab8eb
kelldani <87382954+khalildahbane@users.noreply.github.com>
2023-10-09 09:23:09 +0000

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 if __name__ == "__main__":
     import hidden_4
     for name in dir(hidden_4):
-           if not name.startswith("__"):
+        if not name.startswith("__"):
             print(name)
