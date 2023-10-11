ebef7367be6084717c291b84c1c6b8122c0d0504
khalildahbane <khalildahbne@gmail.com>
2023-10-11 09:56:54 -0700

    Only by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]:
+        multiples = []
+        for num in list:
+            if num % 2 == 0:
+                multiples.append(True)
+            else:
+                (False)
+        return multiples
