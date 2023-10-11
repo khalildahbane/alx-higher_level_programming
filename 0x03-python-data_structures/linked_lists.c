a5de71d23d24659f7c69d18ff84fb6072d4c2b1d
kelldani <87382954+khalildahbane@users.noreply.github.com>
2023-10-11 19:45:44 +0000

    Linked list palindrome

---
+++ 0x03-python-data_structures/linked_lists.c
@@ -0,0 +1,32 @@
+
+    new->n = n;
+    new->next = NULL;
+
+    if (*head == NULL)
+        *head = new;
+    else
+    {
+        while (current->next != NULL)
+            current = current->next;
+        current->next = new;
+    }
+
+    return (new);
+}
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to list to be freed
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+    listint_t *current;
+
+    while (head != NULL)
+    {
+        current = head;
+        head = head->next;
+        free(current);
+    }
+}
