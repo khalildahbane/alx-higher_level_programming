3e48c9c86021c46e083ae7ca2a40ed8a72a2c78c
kelldani <87382954+khalildahbane@users.noreply.github.com>
2023-10-14 15:55:47 +0000

    uniq

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if idx >= 0 and idx < len(my_list):
-        my_list[idx] = element
-    return (my_list)
+def uniq_add(my_list=[]):
+    add = 0
+    for i in set(my_list):
+        add += i
+    return add
