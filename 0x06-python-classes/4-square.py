abb1ae2de2b135ed8b0e06c889e4ccda160f2cfa
kelldani <kelldani100@gmail.com>
2023-10-27 17:31:09 +0100

    Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,18 +8,22 @@ class Square:
 
     def __init__(self, size=0):
         """Initializes sequare"""
-        self.squaer = size
-    
+        self.__size = size
+
+    @property
     def size(self):
         """"Retrieve size"""
-        return self.squaer
-    
+        return self.__size
+
+    @size.setter
     def size(self, value):
         if isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
-            raise TypeError("size must be >= 0")
-        self.squaer = value
+            raise ValueError("size must be >= 0")
+        self.__size = value
         
     def area(self):
+        """Return the curruet square area"""
+        
         return (self.__size * self.__size)
