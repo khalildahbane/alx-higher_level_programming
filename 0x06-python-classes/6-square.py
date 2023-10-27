7708ab5787a894d082f990c532197be8bbb3004c
kelldani <kelldani100@gmail.com>
2023-10-27 22:53:56 +0100

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -27,20 +27,18 @@ class Square:
             raise ValueError("size must be >=0")
         self.__size = value
 
-    @position.setter
+    @property
     def position(self):
-        """set the position of this Square"""
-
-        return self.__position
+        """Getter/setter property for the position of the square."""
+        return (self.__position)
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple):
-            raise TypeError('position must be a tuple of 2 positive integers')
-        if len(value) != 2:
-            raise TypeError('position must be a tuple of 2 positive integers')
-        if len([i for i in value if isinstance(i, int) and i >= 0]) != 2:
-            raise TypeError('position must be a tuple of 2 positive integers')
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
@@ -54,9 +52,9 @@ class Square:
         if self.__size == 0:
             print()
             return
-        
-        [print("") for i in range(0, self.position[1])]
+       
+        [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.position[0])]
+            [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
