dcfc87f6610e74313f75ce0a324d49a653edd171
kelldani <kelldani100@gmail.com>
2023-10-29 13:33:46 +0100

    Print Square instance

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -36,8 +36,8 @@ class Square:
     def position(self, value):
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
+                not all(isinstance(i, int) for i in value) or
+                not all(i >= 0 for i in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
@@ -51,9 +51,8 @@ class Square:
 
         if self.__size == 0:
             print()
-            return
-
-        [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
+        else:
+            for _ in range(self.position[1]):
+                print()
+            for _ in range(self.size):
+                 print(" " * self.position[0] + "#" * self.size)
