760ceb41ef0f609a90135afafb7983e2e92574f1
kelldani <kelldani100@gmail.com>
2023-11-08 12:25:44 +0100

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module contains a function that returns the list of available"""
+
+
+def lookup(obj):
+    """this functions looks for attributes and methods of the given object"""
+
+    return dir(obj)
