816d777b67c6b922e85f6ed726ee7bd411f0a6c8
kelldani <kelldani100@gmail.com>
2023-11-08 16:11:05 +0100

    Inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""A function that returns True if the object is an instance otherwise False"""
+"""Module contains a function that returns true if object is an instance"""
 
 
-def  inherits_from(obj, a_class):
-    """This function returns true if object is an instance otherwise False"""
+def inherits_from(obj, a_class):
+    """This function returns true if object is an instance"""
 
     return (issubclass(type(obj), a_class) and type(obj) != a_class)
