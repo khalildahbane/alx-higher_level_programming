bf127ef733dab2b2739adf32ef660e9280bbbb6a
kelldani <kelldani100@gmail.com>
2023-11-08 14:09:41 +0100

    My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module inherits from the list class"""
+
+class Mylist(list):
+    """a Public instance class"""
+
+    def print_sorted(self):
+        """print a sorted list"""
+
+        print(sorted(self))
