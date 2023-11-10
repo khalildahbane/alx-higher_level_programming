834b636a243affc8130bc7ca81cc664b2bfa4a83
kelldani <kelldani100@gmail.com>
2023-11-10 17:33:24 +0100

    Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,5 @@
 
 def read_file(filename=""):
     """Read and print file"""
-
-    with open(filename, "r" encoding="UTF-8") as file:
-    print (file.read(), end="")
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
