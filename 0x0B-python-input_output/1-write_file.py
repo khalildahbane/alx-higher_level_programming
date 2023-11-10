f2a381a1fe39c343759d834449e85bb48851b2c4
kelldani <kelldani100@gmail.com>
2023-11-10 17:45:44 +0100

    Input/Output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" function that writes a string to a text file (UTF8) and returns the number"""
+"""function that writes a string to a text file UTF8 and returns the number"""
 
 
-def  write_file(filename="", text=""):
+def write_file(filename="", text=""):
     """Writes a string"""
-    with open(filename, "w" , encoding="utf-8")as file:
+    with open(filename, "w", encoding="utf-8")as file:
         return file.write(text)
