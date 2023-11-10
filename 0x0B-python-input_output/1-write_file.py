09cc14be79a4bcb9d84bdc8e198eb272d2195760
kelldani <kelldani100@gmail.com>
2023-11-10 17:42:59 +0100

    Input/Output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,5 @@
 
 def  write_file(filename="", text=""):
     """Writes a string"""
-    try:
-        with open(filename, "w" , encoding="utf-8")as file:
-            return file.write(text)
+    with open(filename, "w" , encoding="utf-8")as file:
+        return file.write(text)
