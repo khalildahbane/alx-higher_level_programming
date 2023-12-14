1dc6a76bfca6853acee8a7d46c2e93fb7d7155c4
kelldani <87382954+khalildahbane@users.noreply.github.com>
2023-12-14 22:05:44 +0100

    SQL

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,2 +1,2 @@
---lists all rows of the table first_table from the database hbtn_0c_0 in MySQL
+-- lists all rows of the table first_table from the database hbtn_0c_0 in MySQL
 SELECT * FROM first_table;
