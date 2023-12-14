7e94256115b16292b2a165285ecf919e48320932
kelldani <87382954+khalildahbane@users.noreply.github.com>
2023-12-14 22:32:03 +0100

    SQL

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
--- display the number of records in the table first_table of the database hbtn_0c_0 in MySQL
-SELECT COUNT(*) FROM first_table;
+-- displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in MySQL
+SELECT COUNT(id) FROM first_table WHERE id = 89;
