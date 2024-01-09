58a916e141512c4c893cfd07d5125f2103fc0c1b
kelldani <87382954+khalildahbane@users.noreply.github.com>
2024-01-09 12:58:43 +0100

    JavaScript

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv;
+    if (args.length === pricess.argv[2]) {
+    console.log('No argument');
+    } else if (args.length === process.argv[3]) {
+        console.log('Argument found');
+    }
+    else {
+        console.log('Arguments found');
+    }
