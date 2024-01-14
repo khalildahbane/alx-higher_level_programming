33690d36f0620e9f223e0ec4f866b6d6decebec4
khalildahbane <khalildahbane@gmail.com>
2024-01-14 16:20:43 +0000

    JavaScript

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-function factorial (n) {
-  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+const { argv } = require('process');
+const a = parseInt(argv[2], 10);
+function factorial (a) {
+  if (a <= 1) return 1;
+  return a * factorial(a - 1);
 }
-
-console.log(factorial(Number(process.argv[2])));
+if (isNaN(a)) console.log(1);
+else console.log(factorial(a));
