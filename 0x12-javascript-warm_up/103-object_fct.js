d39610e596e15cfd93b112eb1e5b24f8becf1ffb
khalil <khalil@gmail.com>
2024-01-14 18:48:53 +0000

    javaScript

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,13 +1,12 @@
-t myObject = {
+#!/usr/bin/node
+const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
-
 myObject.incr = function () {
   this.value++;
 };
-
 myObject.incr();
 console.log(myObject);
 myObject.incr();
