38edc526c09e7b96e192dfc8302bb26ecbf4f78e
khalildahbane <khalildahbane@gmail.com>
2024-01-21 13:15:47 +0000

    JavaScript

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor(w, h) {
-        if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-        }
-    if (w === undefined || h === undefined) {
-        return Rectangle = {};
-        }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 }
 
 module.exports = Rectangle;
