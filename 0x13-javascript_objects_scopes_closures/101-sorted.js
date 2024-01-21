5d9247087c647ae88456b78408564f26cfc1195a
khalildahbane <khalildahbane@gmail.com>
2024-01-21 13:42:30 +0000

    JavaScript

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,4 +1,5 @@
-t dict = require('./101-data').dict;
+#!/usr/bin/node
+const dict = require('./101-data').dict;
 const newDict = {};
 for (const k in dict) {
   if (newDict[dict[k]] === undefined) {
