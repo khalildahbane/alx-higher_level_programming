800c05f5031ba4d85f07d0323bcc6d8561c83f08
khalildahbane <kelldani100@gmail.com>
2024-01-27 18:41:18 +0100

    Python - Network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 """ fin a peak"""
 
+def find_peak(list_of_integers):
+     """def doc"""
+    if list_of_integers:
+        lf = 0
+        r = len(list_of_integers) - 1
+        while lf < r:
+            m = (lf + r) // 2
+            if list_of_integers[m] > list_of_integers[m + 1]:
+                r = m
+            else:
+                lf = m + 1
+        return list_of_integers[lf]
